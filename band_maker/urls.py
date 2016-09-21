13274578e94d5acf3bd4cd97b2b341a9a406a70d
John Rake <john.rake12@gmail.com>
2016-09-21 13:20:42 -0500

    Rename gettingstarted app to band_maker

--- band_maker/urls.py
+++ band_maker/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 import home.views
 
 # Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
+# url(r'^$', 'band_maker.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
