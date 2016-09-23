04592b9f915849d7f61680245a2e15655f89c8b0
John Rake <john.rake12@gmail.com>
2016-09-23 15:36:48 -0500

    Fix heroku deployment server

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: gunicorn band_maker.wsgi --log-file -
--- band_maker/wsgi.py
+++ band_maker/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gettingstarted project.
+WSGI config for band_maker project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
