9252b904ed38c9bc5a91b0689388268551fe9214
John Rake <john.rake12@gmail.com>
2016-10-21 19:11:57 -0500

    Show published and work in progress songs for a users page

--- users/views.py
+++ users/views.py
@@ -10,10 +10,17 @@ from tracks.models import Track
 
 class SongsView(generic.ListView):
     template_name = 'users/songs.html'
-    context_object_name = 'song_list'
+    context_object_name = 'published_song_list'
 
     def get_queryset(self):
-        return Song.objects.filter(created_by__username=self.kwargs['username'])
+        return Song.objects.filter(created_by__username=self.kwargs['username'], published=True)
+
+    def get_context_data(self, **kwargs):
+        context = super(SongsView, self).get_context_data(**kwargs)
+        context['unpublished_song_list'] = Song.objects.filter(created_by__username=self.kwargs['username'],
+                                                               published=False)
+
+        return context
 
 
 class SongCreate(LoginRequiredMixin, generic.CreateView):
