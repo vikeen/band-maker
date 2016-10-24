3e08d1548be8e6c2ce8bbb793a87eaa9771947ec
John Rake <john.rake12@gmail.com>
2016-10-24 16:39:44 -0500

    improve song download feature for logging and os paths

--- songs/views.py
+++ songs/views.py
@@ -1,6 +1,7 @@
 import zipfile
 import os
 import boto3
+import logging
 
 from django.views import generic
 from django.http import HttpResponse
@@ -43,40 +44,34 @@ def download(request, pk):
 
     archive_file_name = '%s.zip' % song.title
     archive_file_path = '%s/%s' % (temp_download_dir, archive_file_name)
+
     archive = zipfile.ZipFile(archive_file_path, 'w')
 
-    try:
+    logging.info('download song: [%s] with title: [%s]' % (song.id, song.title))
 
-        print 'download song: [%s] with title: [%s]' % (song.id, song.title)
+    for track in tracks:
+        s3_track_file_path = '%s/tracks/%s' % (song.created_by, track.media_name)
+        temp_download_file_path = os.path.join(temp_download_dir, track.media_name)
+        logging.info('downloading track [%s] to [%s]' % (s3_track_file_path, temp_download_file_path))
 
-        for track in tracks:
-            s3_track_file_path = '%s/tracks/%s' % (song.created_by, track.media_name)
-            temp_download_file_path = '%s/%s' % (temp_download_dir, track.media_name)
+        s3_client.download_file(
+            Bucket=s3_bucket,
+            Key=s3_track_file_path,
+            Filename=temp_download_file_path)
 
-            print 'downloading track [%s] to [%s]' % (s3_track_file_path, temp_download_file_path)
+        archive.write(temp_download_file_path, track.media_name)
 
-            s3_client.download_file(
-                Bucket=s3_bucket,
-                Key=s3_track_file_path,
-                Filename=temp_download_file_path)
-
-            archive.write(temp_download_file_path, track.media_name)
-
-        print 'zip file created name: [%s] at path: [%s]' % (archive_file_name, archive_file_path)
-    except:
-        rmtree(temp_download_dir)
-    finally:
-        print 'zip file creation complete'
-        archive.close()
+    logging.info('zip file created name: [%s] at path: [%s]' % (archive_file_name, archive_file_path))
+    archive.close()
 
     with File(open(archive_file_path, 'rb')) as f:
         response = HttpResponse(f.chunks())
 
-    print 'clean up temporary download directory [%s]' % temp_download_dir
-
-    rmtree(temp_download_dir)
-
     response['Content-Type'] = 'application/zip'
     response['Content-Disposition'] = 'attachment; filename=%s' % archive_file_name
+
+    logging.info('clean up temporary download directory [%s]' % temp_download_dir)
+
+    rmtree(temp_download_dir)
 
     return response
