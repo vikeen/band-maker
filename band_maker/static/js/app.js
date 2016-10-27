e4a9c574e90eec8726f9eb3c97669d3474f0d00a
John Rake <john.rake12@gmail.com>
2016-10-26 21:38:09 -0500

    Remove track upload widget

--- band_maker/static/js/app.js
+++ band_maker/static/js/app.js
@@ -1,11 +1,9 @@
 import {MediaPlayer} from "./components/media_player";
-import {TrackUpload} from "./components/track_upload";
 import {secondsToDateTime} from "./utils/seconds_to_date_time";
 
 window.bm = {
     components: {
-        MediaPlayer: MediaPlayer,
-        TrackUpload: TrackUpload
+        MediaPlayer: MediaPlayer
     },
     utils: {
         secondsToDateTime: secondsToDateTime
@@ -17,18 +15,4 @@ window.bm = {
  * Initialize application widgets
  */
 $(document).ready(() => {
-    __initializeTrackUpload();
 });
-
-function __initializeTrackUpload() {
-    $("[data-bm-widget='track-upload']").each(function () {
-        const $element = $(this);
-        let track = $element.data("bmWidgetTrackUploadTrack");
-        let song = $element.data("bmWidgetTrackUploadSong");
-
-        track = track ? track[0] : undefined;
-        song = song ? song[0] : undefined;
-
-        new bm.components.TrackUpload($element, track, song);
-    })
-}
