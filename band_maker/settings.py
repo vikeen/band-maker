ef4dc3bba6a764c697c81907a055e260b115df62
John Rake <john.rake12@gmail.com>
2016-11-01 12:52:32 -0500

    Try moving script tags around

--- band_maker/settings.py
+++ band_maker/settings.py
@@ -97,7 +97,7 @@ else:
             'HOST': 'localhost',
             'PORT': ''
         }
-    } 
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
