150846b6af4a061bb5acb06d923e9b8261fe1731
John Rake <john.rake12@gmail.com>
2016-12-09 15:06:47 -0600

    setup configuration for debug mode

--- band_maker/settings.py
+++ band_maker/settings.py
@@ -22,8 +22,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: change this before deploying to production!
 SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['BAND_MAKER_DEBUG']
 
 # Application definition
 
