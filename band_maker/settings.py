0567f8136fd1331fe1112ce9f5cea1dc237fd049
John Rake <john.rake12@gmail.com>
2016-12-13 22:27:05 -0600

    working on travis.ci integration

--- band_maker/settings.py
+++ band_maker/settings.py
@@ -84,7 +84,7 @@ if 'TRAVIS' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'travisci',
+            'NAME': 'band_maker',
             'USER': 'postgres',
             'PASSWORD': '',
             'HOST': 'localhost',
