e29db8e7e1fb2e87c417b312c15b3ca365d8d9b7
John Rake <john.rake12@gmail.com>
2016-12-14 15:49:42 -0600

    force users away from the heroku application homepage

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 
-# Create your views here.
+
+@login_required
+@require_http_methods(['GET'])
 def index(request):
-    return render(request, 'home/index.html')
+    return redirect(reverse('songs:index'))
