be834bc36760ab528ea3d5b26a6081af2d268a7b
John Rake <john.rake12@gmail.com>
2016-12-15 21:53:36 -0600

    Add error logging for heroku

--- band_maker/settings.py
+++ band_maker/settings.py
@@ -112,6 +112,22 @@ else:
         }
     }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
